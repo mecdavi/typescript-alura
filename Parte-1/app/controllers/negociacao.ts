d771fbb46b8b8c7aa3ea20dc42d58a85a1a84084
mecdavi <david.eduard86@gmail.com>
2024-06-01 23:30:14 -0300

    vinculando com o private do js

--- Parte-1/app/controllers/negociacao.ts
+++ Parte-1/app/controllers/negociacao.ts
@@ -3,4 +3,13 @@ export class NegociacaoController{
     private inputQuantidade;
     private inputValor;
 
+    constructor(){
+        this.inputData = document.querySelector('#data')
+        this.inputQuantidade = document.querySelector('#quantidade')
+        this.inputValor = document.querySelector('#valor')
+    }
+
+    adiciona(){
+        console.log(this.inputData,this.inputQuantidade,this.inputValor)
+    }
 }
--- Parte-1/dist/js/controllers/negociacao.js
+++ Parte-1/dist/js/controllers/negociacao.js
@@ -1,2 +1,10 @@
 export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        console.log(this.inputData, this.inputQuantidade, this.inputValor);
+    }
 }
